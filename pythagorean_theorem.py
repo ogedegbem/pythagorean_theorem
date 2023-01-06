d1a75316f5efe04fd782995e1b77cb4af300abab
ogedegbem <83469006+ogedegbem@users.noreply.github.com>
2023-01-06 13:53:26 +0100

    Add files via upload

---
+++ pythagorean_theorem.py
@@ -0,0 +1,9 @@
+import math
+def myPythagorean(a,b):
+    """write a function (code)to calculate the length of an unknown side 
+    of a right triangle, given two sides are known.
+    """
+
+    c = math.sqrt(a**2 + b**2)    # math.sqrt will be used to find the length of the unknown side.
+    return c
+    raise NotImplementedError()
